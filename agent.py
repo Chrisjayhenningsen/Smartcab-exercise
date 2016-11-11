85dfaaf7d610d49b7e443fba81857798cbaba969
Chris Henningsen <chrisjayhenningsen@gmail.com>
2016-11-11 18:02:46 -0400

    test: update flags
    updating flags to test model performance

--- agent.py
+++ agent.py
@@ -53,7 +53,9 @@ class LearningAgent(Agent):
 
         # Collect data about the environment
         waypoint = self.planner.next_waypoint() # The next waypoint 
-        inputs = self.env.sense(self)           # Visual input - intersection light and traffic
+        inputs = self.env.sense(self)           # Visual input - intersection light and 
+
+traffic
         deadline = self.env.get_deadline(self)  # Remaining deadline
 
         ########### 
@@ -171,7 +173,7 @@ def run():
     # Follow the driving agent
     # Flags:
     # enforce_deadline - set to True to enforce a deadline metric
-    env.set_primary_agent(agent)
+    env.set_primary_agent(agent, enforce_deadline = True)
 
     ##############
     # Create the simulation
@@ -183,14 +185,14 @@ def run():
     #self.env.display = False
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env)
+    sim = Simulator(env, display = True, update_delay = 0.01,log_metrics = True)
     
     ##############
     # Run the simulator
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run()
+    sim.run(n_test = 10)
 
 
 if __name__ == '__main__':
